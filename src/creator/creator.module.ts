206e7e95399ac47f7f24ec366b94ee61785fdd6d
Max Korolev <nrjshka@gmail.com>
2022-07-01 20:16:08 +0300

    removed: unused screen

--- src/creator/creator.module.ts
+++ src/creator/creator.module.ts
@@ -2,9 +2,8 @@ import { Module } from '@nestjs/common';
 
 import { CreatorUpdate } from './creator.update';
 import { CreateScene } from './scenes/create.scene';
-import { RandomNumberScene } from './scenes/scene.scene';
 
 @Module({
-  providers: [RandomNumberScene, CreatorUpdate, CreateScene],
+  providers: [CreatorUpdate, CreateScene],
 })
 export class CreatorModule {}
